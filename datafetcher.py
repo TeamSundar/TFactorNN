85c5328bbcff9aee9e38678a0dd79f643b175f9c
Yogesh <ykalakoti@gmail.com>
2021-04-30 10:07:09 +0530

    Routine edits

--- datafetcher.py
+++ datafetcher.py
@@ -23,16 +23,49 @@ class PROCESS:
             url = metadata.loc[row]['File download URL']
             acc = metadata.loc[row]['File accession']
             if acc+'.bed.gz' in os.listdir(datapath):
-                filename = wget.download(url, out=datapath)
+                print('\n%s already downloaded.'%(acc))
             else:
-                print('%s already downloaded.'%(acc))
+                print('\nDownloading %s'%acc)
+                filename = wget.download(url, out=datapath)
+        return None
 
+
+
+    def importMetadata(self):
+        # Prepare metadata
+        metadata = pd.read_csv(self.path+'/metadata.tsv', sep='\t')
+        metadata_ = metadata[['File accession', 'Biosample term name', 'Biosample type', 'File assembly', 'Experiment target']]
+        metadata_ = metadata_[metadata_['Biosample type']=='cell line']
+        return metadata_
+
+    def filterDuplicate(self):
+        metadata = self.importMetadata()
+        # Filte duplicate tf-celline pairs
+        cellline_dict = collections.Counter(metadata_['Biosample term name'])
+        master_record = pd.DataFrame()
+        for line in tqdm(cellline_dict):
+            df = metadata_[metadata_['Biosample term name']==line]
+            tfs = collections.Counter(df['Experiment target'])
+            for tf in tfs:
+                df_ = df[df['Experiment target']==tf]
+                master_record = master_record.append(df_[df_['File accession']==random.choice(df_['File accession'].values)], ignore_index=True)
+        print(master_record.shape)
+        return master_record
+
+    def zip2txt():
+        pass
+        
+
+    def dataSummary(self):
+        master_record = self.filterDuplicate()
+        for acc in tqdm(master_record['File accession'].values):
+            try:
+                data = pd.read_csv('/data/'+acc+'.bed', sep='\t', header=None)
+                list_ = abs(data[1].values-data[2].values)
+                means.append(np.mean(list_))
+            except:
+                print(acc)
+        plt.hist(means, bins=30)
+        print(stats.describe(means))
         #print(metadata_path, datapath)
-
-if __name__ == '__main__':
-    path = sys.argv[1]
-    obj = PROCESS(path)
-    obj.getData()
-
-
-    
+        return None
